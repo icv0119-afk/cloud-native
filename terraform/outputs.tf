540f7527aba0879c518657792c6b3cf71bbda726
Vic <icv0119@gmail.com>
2025-12-25 23:31:32 +0800

    new html

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "public_ip" {
-  description = "EC2 實例的公有 IP"
+  description = "EC2 IP"
   value       = aws_instance.app_server.public_ip
 }
 
 output "ssh_command" {
-  description = "連線指令"
+  description = "連"
   value       = "ssh -i devops-key.pem ec2-user@${aws_instance.app_server.public_ip}"
 }
