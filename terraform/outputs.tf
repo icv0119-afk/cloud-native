9b85d20ec111fe6633afea431c1f33a4a0bcdb41
Vic <icv0119@gmail.com>
2025-12-25 12:51:05 +0800

    ignore

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "public_ip" {
-  description = "EC2 實例的公有 IP (請將此填入 GitHub Secrets 的 EC2_HOST)"
+  description = "EC2 實例的公有 IP"
   value       = aws_instance.app_server.public_ip
 }
 
 output "ssh_command" {
   description = "連線指令"
-  value       = "ssh -i vic-key.pem ec2-user@${aws_instance.app_server.public_ip}"
+  value       = "ssh -i devops-key.pem ec2-user@${aws_instance.app_server.public_ip}"
 }
